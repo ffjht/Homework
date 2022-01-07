e99ac90dabef5b5eebc10575af30329de175f6ce
ffjht123@gmail.com <ffjht123@gmail.com>
2022-01-07 16:20:57 +0900

    최원준

--- Homework220107/Homework220107/Homework220107.cpp
+++ Homework220107/Homework220107/Homework220107.cpp
@@ -15,119 +15,95 @@ bool check_Ip(std::string temp)
 	int cur = 0;
 	for (int i = 0; i < temp.size(); i++)
 	{
+		if (temp[i] == ' ') continue;
 		if (temp[i] < '0' || temp[i] > '9') return false;
-		else cur = 10 * cur + temp[i] - '0';
+		else cur = 10 * cur + int(temp[i] - '0');
 	}
 
 	if (cur > 255) return false;
+	return true;
 }
 
 bool check_port(std::string temp)
 {
 	int cur = 0;
+
 	for (int i = 0; i < temp.size(); i++)
 	{
+		if (temp[i] == ' ') continue;
 		if (temp[i] < '0' || temp[i] > '9') return false;
-		else cur = 10 * cur + temp[i] - '0';
+		else
+		{
+			cur = 10 * cur + temp[i] - '0';
+		}
 	}
 
 	if (cur > 65535) return false;
+	return true;
 }
 
 int main()
 {
-	WSADATA wsa;
-
-	if (0 != WSAStartup(MAKEWORD(2, 2), &wsa))
-	{
-		std::cout << "WSAStartup Error" << std::endl;
-	}
-
-	SOCKET SessionSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	if (INVALID_SOCKET == SessionSocket)
-	{
-		return 0;
-	}
-
-	std::cout << "IP주소를 입력해주세요" << std::endl;
 	std::string Ip;
-	std::cin >> Ip;
-
-	if ("Q" == Ip
-		|| "q" == Ip)
+	int port = -1;
+	while (true)
 	{
-		Ip = "127.0.0.1";
-	}
+		std::cout << "IP주소를 입력해주세요" << std::endl;
+		getline(std::cin, Ip);
 
-	std::stringstream ss(Ip);
-	std::string token;
-	bool isIp = true, isPort = false;
-	int ipCnt = 0;
-	u_short port;
-
-	while (isIp && ipCnt < 4 && std::getline(ss, token, '.'))
-	{
-		ipCnt++;
-		if (token.empty()) isIp = false;
-
-		if (ipCnt < 4)
+		if ("Q" == Ip
+			|| "q" == Ip)
 		{
-			isIp = check_Ip(token);
+			Ip = "127.0.0.1";
 		}
-		else
+
+		std::stringstream ss(Ip);
+		std::string token;
+		bool isIp = true, isPort = false;
+		int ipCnt = 0;
+
+		while (isIp && ipCnt < 4 && std::getline(ss, token, '.'))
 		{
-			std::stringstream ss(token);
-			std::string temp;
-			bool lastIp = true;
-			while (std::getline(ss, temp, ':'))
+			ipCnt++;
+			if (token.empty()) isIp = false;
+
+			if (ipCnt < 4)
 			{
-				if (lastIp)
+				isIp = check_Ip(token);
+			}
+			else
+			{
+				std::stringstream ss(token);
+				std::string temp;
+				bool lastIp = true;
+				while (std::getline(ss, temp, ':'))
 				{
-					isIp = check_Ip(temp);
+					if (lastIp)
+					{
+						isIp = check_Ip(temp);
+					}
+					else
+					{
+						isPort = check_port(temp);
+
+						if (isPort) 
+							port = stoi(temp);
+					}
+					lastIp = false;
 				}
-				else
-				{
-					isPort = check_port(temp);
-					if (isPort) port = stoi(temp);
-				}
-				lastIp = false;
 			}
 		}
+
+		if (!isIp) continue;
+		while(!isPort)
+		{
+			std::string tempPort;
+			std::cout << "포트를 입력해주세요\n";
+			getline(std::cin, tempPort);
+			isPort = check_port(tempPort);
+			if (isPort) port = stoi(tempPort);
+		}
+
+		if (isIp && isPort) break;
 	}
-
-	if (!isIp) return 0;
-	if (!isPort)
-	{
-		std::cout << "포트를 입력해주세요\n";
-		std::cin >> port;
-	}
-
-	SOCKADDR_IN Add = { 0, };
-	Add.sin_family = AF_INET;
-	Add.sin_port = htons(port);							//
-	if (SOCKET_ERROR == inet_pton(AF_INET, Ip.c_str(), &Add.sin_addr))
-	{
-		return 0;
-	}
-	int Len = sizeof(SOCKADDR_IN);
-
-	if (SOCKET_ERROR == connect(SessionSocket, (const sockaddr*)&Add, Len))
-	{
-		return 0;
-	}
-
-	char Buffer[1024] = { 0 };
-
-	int Result = recv(SessionSocket, Buffer, sizeof(Buffer), 0);
-
-	if (SOCKET_ERROR == Result)
-	{
-		return 0;
-	}
-
-	std::cout << "접속에 성공했습니다." << std::endl;
-
-	_getch();
-
 }
